727183d51c27f92695b3128c9472600f1755693d
Haofeng Zhang <hfzhang.cn@gmail.com>
2021-11-10 13:03:06 -0800

    Python

--- python/findFile.py
+++ python/findFile.py
@@ -6,24 +6,30 @@ class findName():
         self.name = name
         self.recursive = recursive
         self.data = self.init_data()
+        # print(self.data)
 
     def init_data(self):
         res = list()
         if self.recursive:
             fullPath = os.walk(self.path)
+            for root,dir,filename in fullPath:
+                # print(root, dir, filename)
+                for name in filename:
+                    name = root + '/' + name
+                    res.append(name)
         else:
             fullPath = os.listdir(self.path)
-        for i in fullPath:
-            res.append(i)
+            for i in fullPath:
+                res.append(i)
         return res       
 
-    def byName(self, name, recursive=False):
+    def byName(self, name):
         res = []
         for i in self.data:
-            if re.match(name, i):
+            if re.search(name, i):
                 res.append(i)
         return res
-    def BiggerThanSize(self, size, recursive=False):
+    def BiggerThanSize(self, size):
         res = dict()
         for i in self.data:
             if os.path.getsize(i) >= size:
@@ -31,7 +37,7 @@ class findName():
         return res
 
 if __name__ == '__main__':
-    mySearch = findName()
+    mySearch = findName(recursive=True)
     res = mySearch.byName('wechat')
     print(res)
     res = mySearch.BiggerThanSize(5000)
